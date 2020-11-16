e33b6a05f7a262e0237759b01ed4cbefe4525e94
fnyaoke <fnyaoke@gmail.com>
2020-11-16 07:13:42 +0300

    starting a new application

--- viewgallery/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-from django.db import models
-
-class Gallery(models.Model):
-    image_name = models.CharField(max_length =30)
-    image_description = models.CharField(max_length =60)
-    image = models.ImageField(upload_to = 'gallery/', default = 'static/images/default.jpg')
